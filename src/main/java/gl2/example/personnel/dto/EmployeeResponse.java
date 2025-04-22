9681edc440d291f4f45e885af5f5746585c410c4
Aymen Abid <aymen.abid20233@gmail.com>
2025-04-22 08:20:25 +0200

    updating Swagger Docs

--- src/main/java/gl2/example/personnel/dto/EmployeeResponse.java
+++ src/main/java/gl2/example/personnel/dto/EmployeeResponse.java
@@ -10,12 +10,12 @@ public class EmployeeResponse {
     @Schema(description = "Unique ID of the employee", example = "1")
     private Long id;
 
-    @Schema(description = "Employee's first name", example = "John")
+    @Schema(description = "Employee's first name", example = "Aymen")
     private String firstName;
 
-    @Schema(description = "Employee's first name", example = "Doe")
+    @Schema(description = "Employee's first name", example = "Abid")
     private String lastName;
 
-    @Schema(description = "Employee's email address", example = "john.doe@example.com")
+    @Schema(description = "Employee's email address", example = "Aymen.Abid@example.com")
     private String email;
 }
